3019a3d342a06876dbb231fa70feffac8cc8aa73
Andrius Jaškauskas <jaskauskas.andrius@gmail.com>
2018-12-03 12:09:30 +0100

    empty GL canvas created

---
+++ path-tracer.js
@@ -0,0 +1,22 @@
+main();
+
+function main()
+{
+    const canvas = document.querySelector("#pathTracer");
+    
+    // Initialize the GL context
+    const gl = canvas.getContext("webgl");
+
+    // Only continue if WebGL is available and working
+    if (gl === null)
+    {
+        alert("Unable to initialize WebGL. Your browser or machine may not support it.");
+        return;
+    }
+
+    // Set clear color to black, fully opaque
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    
+    // Clear the color buffer with specified clear color
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
